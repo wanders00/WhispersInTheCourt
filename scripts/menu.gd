52e789af7e03121d4063e9c954a789aefb652a1a
William Andersson <willand@student.chalmers.se>
2024-03-23 21:10:14 +0100

    update menu script

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
-
-func _on_how_to_play_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/throne_room.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
