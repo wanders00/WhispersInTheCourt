9a8b07e33b5b9bf241f33f8775cd269561049148
schonda <schonda@student.chalmers.se>
2024-03-24 13:23:23 +0100

    small fixes

--- scripts/backgroundMusic.gd
+++ scripts/backgroundMusic.gd
@@ -7,7 +7,7 @@ func _ready():
 	music_player.stream = preload("res://dance-of-gypsy-monkey-giulio-fazio-main-version-01-21-15246.mp3") # Adjust the path to your music file
 	#music_player.loop = true # Enable looping
 	music_player.play()
-	music_player.volume_db = 1.0 # Adjust volume as needed
+	music_player.volume_db = 1.0 # Adjust vo§lume as needed
 
 func play_music(stream):
 	music_player.stream = stream
