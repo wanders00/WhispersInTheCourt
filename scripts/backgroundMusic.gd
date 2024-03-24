0b0ecadfe5b1fd7043b6ce97ccd2597d1810dafb
schonda <schonda@student.chalmers.se>
2024-03-24 13:14:33 +0100

    backgroundMusic done

---
+++ scripts/backgroundMusic.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var music_player = AudioStreamPlayer.new()
+
+func _ready():
+	add_child(music_player)
+	music_player.stream = preload("res://dance-of-gypsy-monkey-giulio-fazio-main-version-01-21-15246.mp3") # Adjust the path to your music file
+	#music_player.loop = true # Enable looping
+	music_player.play()
+	music_player.volume_db = 1.0 # Adjust volume as needed
+
+func play_music(stream):
+	music_player.stream = stream
+	music_player.play()
+
+func stop_music():
+	music_player.stop()
