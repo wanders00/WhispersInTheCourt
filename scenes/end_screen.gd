652632635561a9a41528cf951385940151c0d2e3
William Andersson <willand@student.chalmers.se>
2024-03-24 16:51:50 +0100

    fix restart game

--- scenes/end_screen.gd
+++ scenes/end_screen.gd
@@ -16,8 +16,12 @@ func _ready():
 	
 	if first == 1:
 		label2.text += "\nYou caught the kidnapper ('Crewmate') and saved the Princess!"
+	else:
+		label2.text += "\nAn evildoer kidnapped the Princess!"
 	if second == 1:
 		label2.text += "\nYou caught the murderer ('Sir Geoffrey') and saved the Prince!"
+	else:
+		label2.text += "\nAn evildoer murdered the Prince!"
 	
 	if sum == 2:
 		label2.text += "\nWell done! The Kingdom will prosper thanks to your service!"
@@ -27,8 +31,14 @@ func _ready():
 		label2.text += "\nThe Kingdom will most likely turn to ruin thanks to you.... Good job..."
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func _on_exit_pressed():
+	Global.is_interacting = 0
+	Global.x_pos = 0
+	Global.y_pos = 0
+	Global.is_started = 0
+	Global.game_over = 0
+	Global.reset_map()
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
--- scripts/global_variables.gd
+++ scripts/global_variables.gd
@@ -33,3 +33,21 @@ func update(key, value):
 func get_value(key):
 	return map[key]
 	
+func reset_map():
+	map = {
+	"first" : 0,
+	"throne_room_cat" : 0,
+	"throne_room_princess" : 0,
+	"first_evildoer" : 0,
+	"second_evildoer" : 0,
+	"entrance_duke" : 0,
+	"training_area_cat" : 0,
+	"training_area_prince" : 0,
+	"east_wing_duck" : 0,
+	"east_wing_weapons_master" : 0,
+	"east_wing_female_aristocrat" : 0,
+	"outside_addict" : 0,
+	"west_wing_chill" : 0,
+	"west_wing_bro" : 0,
+	"west_wing_aristocrat" : 0
+}
