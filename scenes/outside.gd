49b693d3d179d5085dbfef79c3e2bdc3f97c6bb0
William Andersson <willand@student.chalmers.se>
2024-03-24 14:29:48 +0100

    outside + interact text fix

--- scenes/outside.gd
+++ scenes/outside.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Player.position.x = Global.x_pos
-	$Player.position.y = Global.y_pos
+	pass
+	#$Player.position.x = Global.x_pos
+	#$Player.position.y = Global.y_pos
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
